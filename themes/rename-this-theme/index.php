fdba7571aa684a8d538bceb83a7e7ddf9c1f6f89
Juan Solo <juan@eyesoreinc.com>
2018-06-27 10:20:28 -0400

    some markup additions

--- themes/rename-this-theme/index.php
+++ themes/rename-this-theme/index.php
@@ -1,35 +1,31 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<?php if (have_posts()) :  ?>
+					<h1>Blog</h1>
+					<?php while (have_posts()) : the_post(); ?>
+					
+						<div class="indiv-post">
+							<div class="post-img"><?php the_post_thumbnail(); ?></div>							
+							<div class="post-inner">
+								<h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+								<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+								<div class="entry">
+									<?php the_excerpt(); ?>
+									<a href="<?php the_permalink(); ?>" class="button">Read More</a>
+								</div>
+							</div>
+							<div class="clear"></div>
+						</div>
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-
-			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-
-			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-
-			<div class="entry">
-				<?php the_content(); ?>
+					<?php endwhile; wp_pagenavi(); ?>
+				<?php else : ?>
+					<h2>Nothing found</h2>
+				<?php endif; ?>
 			</div>
-
-			<div class="postmetadata">
-				<?php the_tags('Tags: ', ', ', '<br />'); ?>
-				Posted in <?php the_category(', ') ?> | 
-				<?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?>
-			</div>
-
 		</div>
-
-	<?php endwhile; ?>
-
-	<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-
-	<?php else : ?>
-
-		<h2>Not Found</h2>
-
-	<?php endif; ?>
-
-<?php get_sidebar(); ?>
+	</div>
 
 <?php get_footer(); ?>
--- themes/rename-this-theme/search.php
+++ themes/rename-this-theme/search.php
@@ -1,29 +1,27 @@
 <?php get_header(); ?>
 
-<div class="container">
-	<div class="row">
-		<div class="col-lg-8">
-			<?php if (have_posts()) : ?>
-				<h2>Search Results</h2>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<?php if (have_posts()) : ?>
 
-				<?php while (have_posts()) : the_post(); ?>
-					<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-						<div class="entry">
-							<h2><?php the_title(); ?></h2>
+					<h1>Search Results for <?php the_search_query(); ?></h1>
+
+					<?php while (have_posts()) : the_post(); ?>
+						<div class="indiv-search">
+							<h3><?php the_title(); ?></h3>
 							<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-							<?php the_excerpt(); ?>
-						</div>
-					</div>
-				<?php endwhile; ?>
-				<?php include (TEMPLATEPATH . '/inc/nav.php' ); ?>
-			<?php else : ?>
-				<h2>No posts found.</h2>
-			<?php endif; ?>
-		</div>
-		<div class="col-lg-4">
-			<?php get_sidebar(); ?>
+							<div class="entry">
+								<?php the_excerpt(); ?>
+								<a href="<?php the_permalink(); ?>" class="button">Read More</a>
+							</div>
+						</div>						
+					<?php endwhile; wp_pagenavi(); ?>
+
+				<?php else : ?>
+					<h2>No posts found.</h2>
+				<?php endif; ?>
+			</div>
 		</div>
 	</div>
-</div>
-
 <?php get_footer(); ?>
